df0a5a5f85f175c769d069555b68c8a646076262
Motohiro Takayama <mootoh@gmail.com>
2013-07-10 09:09:27 +0000

    Adds -u/--url option.

--- grader.js
+++ grader.js
@@ -29,23 +29,23 @@ var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
+    //if(!fs.existsSync(instr)) {
+        //console.log("%s does not exist. Exiting.", instr);
+        //process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    //}
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(content) {
+    return cheerio.load(content);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(content, checksfile) {
+    $ = cheerioHtmlFile(content);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +65,28 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_to_read>', 'url to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var onRead = function(content) {
+	    var checkJson = checkHtmlFile(content, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+    };
+
+    var content = null;
+    if (program.file) {
+        fs.readFile(program.file, function(err, data) {
+            if (err) {
+                console.log("failed in loading file: " + program.file);
+                return;
+            }
+            onRead(data);
+        });
+    } else if (program.url) {
+        var rest = require('restler');
+        rest.get(url_to_read).on('complete', onRead);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
